7383678eea16ff76fa57ea8e7e70b77efe849f01
lenovo changes <sherineak@gmail.com>
2024-03-26 12:13:54 +0530

    New chnages

---
+++ 05_Feb2024/Lab012.py
@@ -0,0 +1,9 @@
+# Take 2 numbers from the user and we want to add them
+
+num1 = int(input("Enter the first number"))
+num2 = int(input("Enter the second number"))
+print(num1)
+print(num2)
+sum = num1 + num2      # also can write  sum = int(num1) + int(num2)
+print(type(sum))
+print(sum)
